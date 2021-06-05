c831d255bc81700f874f075cdbf2e576e5a1fd28
TamirAtia <tamir111994@gmail.com>
2021-06-05 17:19:51 +0300

    rx-colors done

---
+++ Day17/rxjs-colors/src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { combineLatest, Observable } from 'rxjs';
+import { Rgb } from './models/types';
+import {map} from 'rxjs/operators'
+import { GameService } from './services/game.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+myColor$!: Observable<string>;
+compColor$!:Observable<string>;
+
+success$!:Observable<boolean>
+
+constructor(
+  private game:GameService
+  ){}
+
+  private rgbToCss(rgb: Rgb){
+    return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+  }
+
+  ngOnInit(): void{
+    let r$= this.game.getRed();
+    let g$= this.game.getGreen();
+    let b$= this.game.getBlue();
+
+
+  this.myColor$ = combineLatest([r$,g$,b$]).pipe(
+    map(tpl => this.rgbToCss(tpl)));
+
+  this.compColor$ = this.game.getComputerColor().pipe(
+    map(rgb => this.rgbToCss(rgb)));
+
+  this.success$ = combineLatest([this.myColor$, this.compColor$]).pipe(
+    map(tpl => tpl[0] === tpl[1])
+  );
+
+  }
+
+  randomize(){
+    this.game.randomizeColor();
+  }
+}
+
+
---
+++ Day17/rxjs-colors/src/app/components/color-presenter/color-presenter.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-color-presenter',
+  templateUrl: './color-presenter.component.html',
+  styleUrls: ['./color-presenter.component.css']
+})
+export class ColorPresenterComponent {
+
+  @Input()
+  color: string | null = '';
+
+}
